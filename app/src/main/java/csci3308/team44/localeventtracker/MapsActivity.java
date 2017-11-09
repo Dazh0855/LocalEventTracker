f05896ac88dfe7c532a6dda69dcfea477eb027a7
arnabp <arnab.purkayastha@colorado.edu>
2017-11-08 21:54:42 -0700

    Added http GET requests and map pins
    HTTP Get requests added to the map. Pins are dropped automatically for each event.

--- app/src/main/java/csci3308/team44/localeventtracker/MapsActivity.java
+++ app/src/main/java/csci3308/team44/localeventtracker/MapsActivity.java
@@ -47,6 +47,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 */
 package csci3308.team44.localeventtracker;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
 import com.google.android.gms.maps.LocationSource;
@@ -54,20 +58,13 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.GoogleMap.OnCameraIdleListener;
-import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 
-import android.content.Intent;
+import com.android.volley.toolbox.JsonObjectRequest;
+
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.os.Bundle;
 import android.Manifest;
@@ -75,8 +72,12 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuInflater;
-import android.view.Menu;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 /*
  * This shows how to use a custom location source.
@@ -84,6 +85,9 @@ import android.view.Menu;
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
     private TextView mTextMessage;
+    String url = "http://138.197.207.68/api";
+    HashMap<String, String> apiHeaders = new HashMap<String, String>();
+    JSONObject data;
     //setTitle("new title");
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -195,6 +199,23 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap map) {
         mMap = map;
+        JSONObject outObject = new JSONObject();
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.GET, url, outObject, new com.android.volley.Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                MapsActivity.this.data = response;
+                displayData(response);
+            }
+        }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error){
+                Toast.makeText(MapsActivity.this, "No Maps API call could be made", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        MySingleton.getInstance(this).addToRequestQueue(jsObjRequest);
+
+        RequestQueue queue = MySingleton.getInstance(this.getApplicationContext()).getRequestQueue();
 
         // Add a marker Boulder in sydney and move the camera
         LatLng boulder = new LatLng(40.014984,-105.270546);
@@ -215,6 +236,43 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             return;
         }
 
+    }
+
+    private void dropPin(double lat, double lng) {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(lat,lng)).title("Marker"));
+
+    }
+
+    public void displayData(JSONObject dataIN) {
+        //MainActivity.this.data = dataIN;
+        //Toast.makeText(MainActivity.this, data.toString(), Toast.LENGTH_LONG).show();
+
+        try {
+            JSONObject events = data.getJSONObject("events");
+            int num = events.getJSONArray("event").length();
+            for ( int i=0; i<=num; i++ ) {
+                Object dat = events.getJSONArray("event").get(i);
+                double lat = new Double(((JSONObject) dat).get("latitude").toString());
+                double lng = new Double(((JSONObject) dat).get("longitude").toString());
+
+                dropPin(lat, lng);
+            }
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            Object dat = dataIN.getJSONArray("event").get(1);
+            dat = (JSONObject) dat;
+            double lat = new Double(((JSONObject) dat).get("lat").toString());
+            double lng = new Double(((JSONObject) dat).get("lng").toString());
+
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
 
